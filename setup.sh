fce57ada9b6c393a28d3d876fdec56b30a5e9912
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2015-08-08 11:40:09 +0200

    add git config to setup script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,19 @@ ln -s "$DIR/.vimrc" "$HOME/.vimrc"
 ln -s "$DIR/.bashrc" "$HOME/.bashrc" 
 ln -s "$DIR/.zshrc" "$HOME/.zshrc"
 ln -s "$DIR/.i3" "$HOME/.i3"
+
+GIT=false
+which git > /dev/null && GIT=true
+
+if $GIT; then
+  echo 'configuring git...'
+  echo -n 'username: ' && read USERNAME
+  git config --global user.name "$USERNAME"
+  echo -n 'email: ' && read EMAIL
+  git config --global user.email "$EMAIL"
+  git config --global core.editor vim
+  git config --global push.default simple
+  git config --global merge.tool vimdiff
+else
+  echo 'git is not installed'
+fi
