1756735df8ee8d159d50bad98913a70be8444899
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-03-19 18:24:17 +0100

    Switch to base16-solarized
    * Switch Xresoursec, i3-config, i3status and vimrc to solarized
    * Improve adding new workspaces using dmenu
    * Change dmenu font to 'DejaVu Sans Mono'

--- .i3/workspace.sh
+++ .i3/workspace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-i3-msg workspace "$(echo 'workspace' | dmenu)"
+i3-msg workspace "$(dmenu.xft -p 'New workspace:' -fn 'DejaVu Sans Mono-11' -nb '#073642' -nf '#eee8d5' -sb '#b58900' -sf '#002b36' -i < /dev/null)"
