c317588700b8f310266b08f72194174924830d06
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-05-20 10:41:54 +0200

    Change workspace moving behavior
    * Make workspace.sh move the active window to the new workspace
    + Add shortcuts to i3 to be able to move the active window to the next/
      previous workspace

--- .i3/workspace.sh
+++ .i3/workspace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-i3-msg workspace "$(dmenu.xft -p 'New workspace:' -fn 'DejaVu Sans Mono-11' -nb '#101010' -nf '#b9b9b9' -sb '#b9b9b9' -sf '#101010' -i < /dev/null)"
+i3-msg move workspace "$(dmenu.xft -p 'New workspace:' -fn 'DejaVu Sans Mono-11' -nb '#101010' -nf '#b9b9b9' -sb '#b9b9b9' -sf '#101010' -i < /dev/null)"
