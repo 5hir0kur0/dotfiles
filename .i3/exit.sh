2032e76563783688e96216b094d6154fc29c7b7f
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2016-07-27 18:30:19 +0200

    exit.sh: Add prompt to confirm exit
    + Add prompt (i3-nagbar) to exit.sh to ask if the user really wants to
      exit when programs don't close
    + Add shortcut to VIM to toggle paste
    * Make the screenshot file generated by pixel_screenshot_lock.sh hidden

--- .i3/exit.sh
+++ .i3/exit.sh
@@ -1,25 +1,64 @@
-#!/bin/sh
+#!/bin/bash
+
+
+THIS="`dirname $0`/$0" # path to script
+# X clients that should be ignored
+WHITELIST=(ibus-x11 ibus-ui-gtk3 unity-settings-daemon notify-osd \
+    gnome-screensaver mozc_renderer redshift-gtk) 
+HOSTNAME=`hostname`
 
 lock() {
-    #gnome-screensaver-command -l
     ~/.i3/pixel_screenshot_lock.sh
+}
+
+# the ugliest implementation of join you have ever seen...
+join_comma() {
+    local IFS=','
+    echo "$*" | sed 's/,/, /g'
+}
+
+listclients() {
+    declare -ag CLIENTS
+    CLIENTS=()
+    local INDEX=0
+    while read LINE; do
+        local CLIENT=`echo $LINE | sed -rn 's/^\S+\s+(.+)$/\1/p'`
+        if [ -n "$CLIENT" ]; then
+            for IGNORED in "${WHITELIST[@]}"; do
+                if [ "$CLIENT" = "$IGNORED" ]; then continue 2; fi
+            done
+            CLIENTS[$INDEX]="$CLIENT"
+            INDEX=$((INDEX + 1))
+        fi
+    done < <(xlsclients) # the loop somehow can't modify the variables
+                         # if I just pipe this in...
+    join_comma "${CLIENTS[@]}"
+}
+
+countclients() {
+    listclients >& /dev/null
+    echo ${#CLIENTS[@]}
 }
 
 killapps() {
     i3-msg '[class=".*"] kill' # close all windows
     while pgrep -f '/usr/bin/anki'; do sleep '0.1'; done # wait for anki to sync
-    HOSTNAME=`hostname`
-    while [ `xlsclients | grep -vE "^$HOSTNAME\s*(ibus-x11|unity-settings-daemon|notify-osd|gnome-screensaver|mozc_renderer)?\s*$" | wc -l` -gt 0 ]; do sleep '0.1'; done
+    if [ `countclients` -gt 0 ]; then # there are clients that refuse to die
+        i3-nagbar -t warning \
+            -m "The following clients refused to close: `listclients`" \
+            -b 'Logout' 'i3-msg exit' \
+            -b 'Shutdown' "/bin/bash $THIS shutdown_force" \
+            -b 'Reboot' "/bin/bash $THIS reboot_force" &
+    fi
+    while [ `countclients` -gt 0 ]; do sleep '0.1'; done
     return 0
 }
 
 my_shutdown() {
-    killapps
     /usr/bin/dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
 }
 
 my_reboot() {
-    killapps
     /usr/bin/dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
 }
 
@@ -34,14 +73,29 @@ case "$1" in
     logout)
         killapps; i3-msg exit
         ;;
+    logout_force)
+        i3-msg exit
+        ;;
     suspend)
         lock; my_suspend
         ;;
     reboot)
+        killapps; my_reboot
+        ;;
+    reboot_force)
         my_reboot
         ;;
     shutdown)
+        killapps; my_shutdown
+        ;;
+    shutdown_force)
         my_shutdown
+        ;;
+    listclients)
+        listclients
+        ;;
+    countclients)
+        countclients
         ;;
     *)
         echo "Usage: $0 {lock|logout|suspend|reboot|shutdown}"
--- .i3/pixel_screenshot_lock.sh
+++ .i3/pixel_screenshot_lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -scale 10% -scale 1000% /tmp/screen_locked.png
-i3lock -i /tmp/screen_locked.png
+scrot /tmp/.screen_locked.png
+convert /tmp/.screen_locked.png -scale 10% -scale 1000% /tmp/.screen_locked.png
+i3lock -i /tmp/.screen_locked.png
