6827fb97a16ae5b68e88ebf7b4ae5daa4e89e1ad
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-02-07 17:22:15 +0100

    ranger: add :jc and remap <c-j>
    + add command "jc" to autojump to a child path of the current directory
    * remap <c-j> (previously <CR>) to execute console jc%space (cf. J)

--- ranger/commands.py
+++ ranger/commands.py
@@ -1,5 +1,6 @@
 from ranger.api.commands import *
 import subprocess
+from os import getcwd
 
 # autojump integration
 # stolen from https://github.com/ranger/ranger/issues/91#issuecomment-231938613
@@ -12,6 +13,19 @@ class j(Command):
 
     def execute(self):
         directory = subprocess.check_output(["autojump"] + self.args[1:])
+        directory = directory.decode("utf-8", "ignore")
+        directory = directory.rstrip("\n")
+        self.fm.execute_console("cd " + directory)
+
+class jc(Command):
+    """
+    :j <directory>
+
+    Uses autojump to set the current directory.
+    """
+
+    def execute(self):
+        directory = subprocess.check_output(["autojump", getcwd()] + self.args[1:])
         directory = directory.decode("utf-8", "ignore")
         directory = directory.rstrip("\n")
         self.fm.execute_console("cd " + directory)
