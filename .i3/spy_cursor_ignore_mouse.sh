5ece63cf3e866ed1321afa31cfbb50d94d1c7553
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-02-19 11:30:50 +0100

    spy_cursor*.sh: add spy_cursor_ignore_mouse.sh
    + add explaining comment to spy_cursor.sh
    + add new script spy_cursor_ignore_mouse.sh

---
+++ .i3/spy_cursor_ignore_mouse.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# position the mouse in the center of a window, when it is focused by the wm
+# (do nothing if the window was focused by moving the mouse)
+# requirements: xdotool
+
+mouse_location() {
+    eval "`xdotool getmouselocation --shell`"
+    echo "$X|$Y"
+}
+
+xprop -spy -root _NET_ACTIVE_WINDOW | while read; do
+    # see if the mouse is moving; this is fairly hacky, but it's the only thing
+    # that I could get to work...
+
+    # this fixes weird behavior when switing to an empty i3 workspace
+    if ! xprop -id "`xdotool getwindowfocus`" | grep -q '^WM_NAME'; then
+        continue
+    fi
+
+    MOUSE_OLD="`mouse_location`"
+    sleep 0.0042
+    MOUSE_NEW="`mouse_location`"
+
+    if [ "$MOUSE_OLD" = "$MOUSE_NEW" ]; then
+        ~/.i3/center_cursor.sh
+    fi
+done
