9ecc7649b48fb0a5f02b8ef6af691b7f12d75095
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-02-28 13:59:34 +0100

    rofiweb: rename to rofisurfraw.sh

---
+++ .i3/rofisurfraw.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# requirements: surfraw, xdotool, xprop
+
+# inspired by: https://github.com/gotbletu/shownotes/blob/master/rofi_websearch_bookmarks_surfraw.md
+
+ELVI_DIRECTORIES=("/usr/lib/surfraw/" "$HOME/.config/surfraw/elvi/")
+BOOKMARKS="$HOME/.config/surfraw/bookmarks"
+OPENSCRIPT="$HOME/.i3/openbrowser.sh"
+
+set -eu
+
+list_elvi() {
+    ARRAY=()
+    for DIR in "${ELVI_DIRECTORIES[@]}"; do
+        ARRAY+=( ${DIR}* )
+    done
+    printf "%s\n" "${ARRAY[@]##*/}" | sort -n
+}
+
+list_bookmarks() {
+    sed '/^$/d' < "$BOOKMARKS" | sort -n
+}
+
+list_both() {
+    (list_elvi; list_bookmarks | cut -f 1 -d ' ') | sort -n
+}
+
+MSG='(swapped tab and c-space)'
+ROFI_ARGS=" -kb-row-select Tab -kb-row-tab Control+space -dmenu -i \
+    -levenshtein-sort -matching fuzzy"
+
+# call with list function as first argument
+get_surfraw_url() {
+    PRM='web:'
+    # lack of double quotes intentional
+    ARG="$("$1" | rofi $ROFI_ARGS -p "$PRM" -mesg "$MSG")"
+    [ -n "$ARG" ] && SURFRAW_print=yes surfraw $ARG
+}
+
+get_bookmark() {
+    PRM='bkm:'
+    ARG="$(list_bookmarks | rofi $ROFI_ARGS -p "$PRM" -mesg "$MSG")"
+    [ -n "$ARG" ] && tr -s ' ' <<< "$ARG" | cut -f 2 -d ' '
+}
+
+case "$1" in
+    bookmark)
+        ARG="$(get_bookmark)"
+        [ -n "$ARG" ] && $OPENSCRIPT "$ARG"
+        ;;
+    search)
+        ARG="$(get_surfraw_url list_elvi)"
+        [ -n "$ARG" ] && $OPENSCRIPT "$ARG"
+        ;;
+    both)
+        ARG="$(get_surfraw_url list_both)"
+        [ -n "$ARG" ] && $OPENSCRIPT "$ARG"
+        ;;
+    nothing)
+        ;;
+    *)
+        echo "usage: $0 bookmark|search|both"
+        ;;
+esac
