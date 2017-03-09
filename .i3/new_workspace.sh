224a54a1194f2d14c32323e26482db61f4e5c13a
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-03-09 15:46:55 +0100

    new_workspace.sh: use lower numbers if possible
    * if there are lower numbers available than the currently lowest
      workspace then use the lowest workspace minus one for the new
      workspace

--- .i3/new_workspace.sh
+++ .i3/new_workspace.sh
@@ -2,15 +2,28 @@
 
 set -eu
 
-last_workspace() {
+list_workspaces() {
     i3-msg -t get_workspaces | grep --color=never -oP '(?<="name":")\d+?(?=")' \
-        | sort --numeric-sort | tail -1
+        | sort --numeric-sort
+}
+
+last_workspace() {
+    list_workspaces | tail -1
+}
+
+first_workspace() {
+    list_workspaces | head -1
 }
 
 LAST="$(last_workspace)"
+FIRST="$(first_workspace)"
 
-if [ -n "$LAST" ]; then
-    i3-msg "${1-}" workspace "$((LAST+1))"
+if [ -n "$LAST" ]; then # if LAST exists, FIRST also exists
+    if [ "$FIRST" -ge 2 ]; then
+        i3-msg "${1-}" workspace "$((FIRST-1))"
+    else
+        i3-msg "${1-}" workspace "$((LAST+1))"
+    fi
 else
     i3-msg "${1-}" workspace "1"
 fi
