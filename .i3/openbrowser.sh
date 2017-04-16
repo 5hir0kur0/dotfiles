a0d403702b6a26d80810f796ce15535f02b738eb
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-04-16 14:35:27 +0200

    openbrowser.sh: new tab support, auto escape
    * if the window is titled "new tab" then open the url in the current
      tab instead of creating a new one
    * press escape before any actions to get out of input fields, etc.

--- .i3/openbrowser.sh
+++ .i3/openbrowser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -u -o pipefail
+shopt -s nocasematch
+
 BROWSER='firefox'
 BROWSER_CLASSES='chromium|firefox|tor browser|navigator'
 
@@ -24,15 +27,24 @@ open_in_default_browser_or_current_window() {
 
     if grep -qiE "$BROWSER_CLASSES" <<< "$CLASSES"; then
         NAME="$(get_property $CURRENT_ID WM_NAME)"
+        xdotool key --window "$CURRENT_ID" Escape
         if [[ "$NAME" =~ .*Vimperator ]]; then
             xdotool windowfocus "$CURRENT_ID" # just to be sure
-            xdotool type --window "$CURRENT_ID" ':tabopen ' "$@"
+            if [[ "$NAME" =~ New[[:space:]]*Tab.* ]]; then
+                xdotool type --window "$CURRENT_ID" ':open ' "$@"
+            else
+                xdotool type --window "$CURRENT_ID" ':tabopen ' "$@"
+            fi
             xdotool key --window "$CURRENT_ID" Return
         else
             # the hackiest hack you have ever seen to open the argument in the
             # currently focused browser window
             xdotool windowfocus "$CURRENT_ID" # just to be sure
-            xdotool key --window "$CURRENT_ID" ctrl+t
+            if [[ "$NAME" =~ New[[:space:]]*Tab.* ]]; then
+                xdotool key --window "$CURRENT_ID" ctrl+l
+            else
+                xdotool key --window "$CURRENT_ID" ctrl+t
+            fi
             xdotool type --window "$CURRENT_ID" "$@"
             # somehow one return just isn't enough...
             xdotool key --window "$CURRENT_ID" Return
