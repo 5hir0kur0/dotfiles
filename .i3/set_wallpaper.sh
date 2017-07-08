9e175d8f8d76498e45c21d079e55d08bf706a325
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-07-08 18:06:39 +0200

    i3/set_wallpaper.sh: add undo stack for wallpapers
    + set_wallpaper.sh now keeps track of past wallpapers and can revert
      to them using 'pop'
    + add binding mod+shift+ctrl+w to pop wallpaper stack

--- .i3/set_wallpaper.sh
+++ .i3/set_wallpaper.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-wallpapers=(~/pics/wall/*)
-feh --bg-fill "${wallpapers[RANDOM % ${#wallpapers[@]}]}"
+set -euo pipefail
+
+HISTORY=/tmp/.wallpaper_history
+
+WALLPAPER=
+
+if [ "${1:-}" = 'pop' ] && [ -f $HISTORY ] && \
+   [ "$(wc -l $HISTORY | cut -f1 -d\ )" -ge 1 ]; then
+    sed -i '$d' $HISTORY # delete comes first b/c last line is current wallpaper
+    WALLPAPER=$(tail -1 $HISTORY)
+else
+    WALLPAPERS=(~/pics/wall/*)
+    WALLPAPER=${WALLPAPERS[RANDOM % ${#WALLPAPERS[@]}]}
+    echo "$WALLPAPER" >> $HISTORY
+fi
+
+feh --bg-fill "$WALLPAPER" &
