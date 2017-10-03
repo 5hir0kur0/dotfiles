4239b844bf9bab6207a1a9ced9575ccf057ee64a
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-10-03 20:49:04 +0200

    bspwm_scratchpad.sh: add ability to hide nodes

--- scripts/.local/share/scripts/bspwm_scratchpad.sh
+++ scripts/.local/share/scripts/bspwm_scratchpad.sh
@@ -1,13 +1,48 @@
-#!/usr/bin/sh
+#!/bin/bash
 
 SCRATCHCLASS=scratchpad-terminal
 
-if ! xdotool search --class --classname "$SCRATCHCLASS" > /dev/null 2>&1; then
-    st -c "$SCRATCHCLASS" -g 130x60 -e tmux new-session -As0 &
-    sleep 0.1
-fi
+ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
 
-ID=$(xdotool search --class --classname $SCRATCHCLASS | head)
+list_hidden_no_scratchpad() {
+    if [ -n "$ID" ]; then
+        bspc query -N -n .hidden | grep -v "$(printf 0x%x "$ID")"
+    else
+        bspc query -N -n .hidden
+    fi
+}
 
-bspc node "$ID" --flag hidden
-bspc node -f "$ID"
+list_floating() {
+    bspc query -N -n '.floating.!hidden.local.leaf'
+}
+
+case "$1" in
+    terminal)
+        if ! xdotool search --class --classname "$SCRATCHCLASS" > /dev/null 2>&1; then
+            st -c "$SCRATCHCLASS" -g 130x60 -e tmux new-session -As0 &
+            sleep 0.1
+        fi
+
+        ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
+
+        bspc node "$ID" --flag hidden
+        bspc node -f "$ID"
+        ;;
+    hide)
+        bspc node --flag hidden
+        ;;
+    hide_floating)
+        list_floating | while read -r F_ID; do
+            bspc node "$F_ID" --flag hidden
+        done
+        ;;
+    unhide)
+        list_hidden_no_scratchpad | while read -r H_ID; do
+            bspc node "$H_ID" --flag hidden
+            bspc node "$H_ID" --state floating
+        done
+        ;;
+    *)
+        echo "usage: $0 {terminal|hide|hide_floating|unhide}" 1>&2
+        ;;
+esac
