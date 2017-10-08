e33b52a0e8f367a03ac87d3a56c8bcfc35e399ba
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-10-08 15:49:59 +0200

    add polybar_modestring.sh
    This script can be used in polybar to emulate i3's modes.
    It looks for a mode name / documentation in the sxhkd config file
    and displays it whenever sxhkd starts a chain.
    
    It can be used with polybar like this:
        [module/showmode]
        type = custom/script
        tail = true
        exec = polybar_modestring.sh

---
+++ scripts/.local/share/scripts/polybar_modestring.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+FIFO_PATH=/tmp/.sxhkd
+SXHKD_PATH=~/.config/sxhkd/sxhkdrc
+MODE=
+MODE_PREV=
+PREV=
+
+find_docstring() {
+    MODE=$(grep -FB2 "$1" $SXHKD_PATH | grep -F '# MODE: ' | cut -d' ' -f3-)
+}
+
+while read -r LINE; do
+    if [[ "$LINE" =~ ^EEnd\ chain ]]; then
+        MODE=
+    elif [[ "$LINE" =~ ^BBegin\ chain ]]; then
+        find_docstring "${PREV#H}"
+    fi
+    if [[ "$MODE" != "$MODE_PREV" ]]; then
+        echo "$MODE"
+    fi
+    PREV=$LINE
+    MODE_PREV=$MODE
+done < $FIFO_PATH
