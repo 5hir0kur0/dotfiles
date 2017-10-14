40cd6bfb43fd6e7582966d3b8fe77adf82f14bdc
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-10-14 19:52:54 +0200

    center_window.sh: use current monitor's resolution

--- scripts/.local/share/scripts/center_window.sh
+++ scripts/.local/share/scripts/center_window.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+
+monitor_info() {
+    xrandr | grep -F "$(bspc query -M -m focused --names)" -A3 | grep -F '*'
+}
+
 eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
-SCREEN_WIDTH=$(xrandr | grep -F '*' | grep -oP '\d+x\d+' | cut -dx -f1)
-SCREEN_HEIGHT=$(xrandr | grep -F '*' | grep -oP '\d+x\d+' | cut -dx -f2)
+SCREEN_WIDTH=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f1)
+SCREEN_HEIGHT=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f2)
 
 xdotool windowmove "$WINDOW" "$(( (SCREEN_WIDTH / 2) - (WIDTH / 2) ))" \
     "$(( (SCREEN_HEIGHT / 2) - (HEIGHT / 2) ))"
