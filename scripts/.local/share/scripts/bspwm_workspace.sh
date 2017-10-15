4eecde5d6a4f6953ee925048aca454941bce74f9
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2017-10-15 15:33:00 +0200

    bspwm_workspace.sh: fix non-numeric workspace bugs

--- scripts/.local/share/scripts/bspwm_workspace.sh
+++ scripts/.local/share/scripts/bspwm_workspace.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 
 list_workspaces() {
-    bspc query -D --names | sort -n
+    bspc query -D --names | sort
+}
+
+list_non_numeric_workspaces() {
+    bspc query -D --names | grep -vP '^\d+$' | sort
 }
 
 list_numeric_workspaces() {
-    bspc query -D --names | grep -P '\d+' | sort -n
+    bspc query -D --names | grep -P '^\d+$' | sort
 }
 
 create_numeric() {
@@ -22,41 +26,59 @@ create_numeric() {
 
 create_if_nonexistent() {
     list_workspaces | grep "^$1\$" || bspc monitor -a "$1"
+    reorder
 }
 
 switch_to() {
     create_if_nonexistent "$1"
     bspc desktop -f "$1"
+    reset
 }
 
 move_to() {
     create_if_nonexistent "$1"
     bspc node -d "$1"
+    reset
 }
 
-join_by() { local IFS="$1"; shift; echo "$*"; }
+reorder() {
+    readarray -t DESKTOPS < <(bspc query --names -D -d -m focused | sort)
+    bspc monitor -o "${DESKTOPS[@]}"
+}
 
 reset() {
-    readarray -t DESKTOPS < <(cat <(bspc query --names -D -d '.occupied' -m focused) <(bspc query --names -D -d -m focused) | sort | uniq)
+    reorder
+    readarray -t DESKTOPS < <(cat \
+        <(bspc query --names -D -d '.occupied' -m focused) \
+        <(bspc query --names -D -d -m focused) | awk '!seen[$0]++')
+    # awk command stolen from: https://unix.stackexchange.com/a/11941
     bspc monitor -d "${DESKTOPS[@]}"
 }
 
 case "${1:?}" in
     switch)
         switch_to "${2:?}"
-        reset
         ;;
     move)
         move_to "${2:?}"
-        reset
+        ;;
+    switch_rofi)
+        WORKSPACE="$(list_non_numeric_workspaces | rofi -dmenu -i -p 'workspace:')"
+        if [ -n "$WORKSPACE" ]; then
+            switch_to "$WORKSPACE"
+        fi
+        ;;
+    move_rofi)
+        WORKSPACE="$(list_non_numeric_workspaces | rofi -dmenu -i -p 'move to workspace:')"
+        if [ -n "$WORKSPACE" ]; then
+            move_to "$WORKSPACE"
+        fi
         ;;
     switch_new)
         switch_to "$(create_numeric)"
-        reset
         ;;
     move_new)
         move_to "$(create_numeric)"
-        reset
         ;;
     reset)
         reset
