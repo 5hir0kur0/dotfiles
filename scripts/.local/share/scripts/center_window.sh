39a78510be2f7e5b549bd6766f5d8497f2f9add4
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-02-15 19:55:11 +0100

    sxhkd: add center mode, tweak window resizing
    center_window.sh: add additional commands for moving the window
                      completely to the left, right, top or bottom

--- scripts/.local/share/scripts/center_window.sh
+++ scripts/.local/share/scripts/center_window.sh
@@ -9,5 +9,21 @@ eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
 SCREEN_WIDTH=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f1)
 SCREEN_HEIGHT=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f2)
 
-xdotool windowmove "$WINDOW" "$(( (SCREEN_WIDTH / 2) - (WIDTH / 2) ))" \
-    "$(( (SCREEN_HEIGHT / 2) - (HEIGHT / 2) ))"
+case "$1" in
+    ''|center)
+        xdotool windowmove "$WINDOW" "$(( (SCREEN_WIDTH / 2) - (WIDTH / 2) ))" \
+            "$(( (SCREEN_HEIGHT / 2) - (HEIGHT / 2) ))"
+        ;;
+    left)
+        xdotool windowmove "$WINDOW" 0 "$Y"
+        ;;
+    right)
+        xdotool windowmove "$WINDOW" "$(( SCREEN_WIDTH - WIDTH ))" "$Y"
+        ;;
+    top)
+        xdotool windowmove "$WINDOW" "$X" 0
+        ;;
+    bottom)
+        xdotool windowmove "$WINDOW" "$X" "$(( SCREEN_HEIGHT - HEIGHT ))"
+        ;;
+esac
