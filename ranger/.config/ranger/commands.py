4db5e771840a8d1817f2278bae42f6a843c4df8e
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-03-08 08:35:56 +0100

    ranger: fasd and fzf integration

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -29,3 +29,43 @@ class jc(Command):
         directory = directory.decode("utf-8", "ignore")
         directory = directory.rstrip("\n")
         self.fm.execute_console("cd " + directory)
+
+# fasd/fzf integration (stolen from: https://github.com/gotbletu/shownotes/blob/master/ranger_fasd_fzf.md)
+
+
+# fzf_fasd - Fasd + Fzf + Ranger (Interactive Style)
+class fzf(Command):
+    """
+    :fzf
+
+    Jump to a file or folder using fzf
+
+    URL: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess, os
+        command="locate $PWD | fzf --height=100%"
+        fzf = self.fm.execute_command(command, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.decode('utf-8').rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+# Fasd with ranger (Command Line Style)
+# https://github.com/ranger/ranger/wiki/Commands
+class fasd(Command):
+    """
+    :fasd
+
+    Jump to directory using fasd
+    URL: https://github.com/clvv/fasd
+    """
+    def execute(self):
+        import subprocess
+        arg = self.rest(1)
+        if arg:
+            directory = subprocess.check_output(["fasd", "-d"]+arg.split(), universal_newlines=True).strip()
+            self.fm.cd(directory)
