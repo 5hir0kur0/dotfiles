3791964a811d145ebb964c0f28bac0c6ae2b9cda
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-03-08 08:42:00 +0100

    blured_screenshot_lock.sh: use ffmpeg, set colors

--- scripts/.local/share/scripts/blured_screenshot_lock.sh
+++ scripts/.local/share/scripts/blured_screenshot_lock.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x10 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+colors=("--textcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "--insidecolor=1d202180" \
+        "--ringcolor=504945aa" "--linecolor=28282800" "--keyhlcolor=f9f5d7ee" \
+        "--ringvercolor=458588ff" "--separatorcolor=28282800" \
+        "--insidevercolor=1d2021aa" "--ringwrongcolor=cc241dff" \
+        "--insidewrongcolor=fb4934aa" "--datecolor=f9f5d7dd")
+
+scrot -q 100 /tmp/.screen_locked.png
+#mogrify -blur 21x21 /tmp/.screen_locked.png
+ffmpeg -i /tmp/.screen_locked.png -vf 'curves=vintage, boxblur=6:6:lr=6:ar=6, curves=increase_contrast' -y /tmp/.screen_locked.png
+
+pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=320 --datestr='%F' -k \
+    --datesize=32 --timesize=64 --timefont=Inconsolata --datefont=Inconsolata \
+    --indicator --veriftext="..." --wrongtext="-_-" "${colors[@]}" \
+    --datepos='tx : ty + 50' --modsize=32 --textsize=32
