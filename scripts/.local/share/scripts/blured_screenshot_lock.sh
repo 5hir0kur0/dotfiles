b4479d36b3ed76ae63acd3254303f07ea5cfdf39
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-03-21 07:47:24 +0100

    add toemacs.sh
    add a script that brings me to emacs or starts it if it's not running
    (works only with i3 for now)

--- scripts/.local/share/scripts/blured_screenshot_lock.sh
+++ scripts/.local/share/scripts/blured_screenshot_lock.sh
@@ -8,9 +8,9 @@ colors=("--textcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "--insidecolor=1d202180" \
 
 scrot -q 100 /tmp/.screen_locked.png
 #mogrify -blur 21x21 /tmp/.screen_locked.png
-ffmpeg -i /tmp/.screen_locked.png -vf 'curves=vintage, boxblur=6:6:lr=6:ar=6, curves=increase_contrast' -y /tmp/.screen_locked.png
+ffmpeg -i /tmp/.screen_locked.png -vf 'curves=increase_contrast, boxblur=9:9:lr=6:ar=6, curves=vintage' -y /tmp/.screen_locked.png
 
-pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=320 --datestr='%F' -k \
-    --datesize=32 --timesize=64 --timefont=Inconsolata --datefont=Inconsolata \
+pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=240 --datestr='%F' -k \
+    --datesize=30 --timesize=48 --timefont=Inconsolata --datefont=Inconsolata \
     --indicator --veriftext="..." --wrongtext="-_-" "${colors[@]}" \
-    --datepos='tx : ty + 50' --modsize=32 --textsize=32
+    --datepos='tx : ty + 35' --modsize=32 --textsize=32
