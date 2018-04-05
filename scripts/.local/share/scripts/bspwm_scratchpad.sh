d94eb700333d704a9e4cbf5c420b0d505282f566
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-04-05 13:52:41 +0200

    toemacs.sh: make it work with bspwm
    * small fixes in bspwm_scratchpad.sh
    * bind toemacs.sh to mod + semicolon in sxhkd

--- scripts/.local/share/scripts/bspwm_scratchpad.sh
+++ scripts/.local/share/scripts/bspwm_scratchpad.sh
@@ -6,7 +6,7 @@ ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
 
 list_hidden_no_scratchpad() {
     if [ -n "$ID" ]; then
-        bspc query -N -n .hidden | grep -v "$(printf 0x%x "$ID")"
+        bspc query -N -n .hidden | grep -vi "$(printf 0x0*%x "$ID")"
     else
         bspc query -N -n .hidden
     fi
@@ -16,6 +16,8 @@ list_floating() {
     bspc query -N -n '.floating.!hidden.local.leaf'
 }
 
+DESKTOP="$(bspc query -D -d '.focused' -m focused)"
+
 case "$1" in
     terminal)
         if ! xdotool search --class --classname "$SCRATCHCLASS" > /dev/null 2>&1; then
@@ -24,7 +26,6 @@ case "$1" in
         fi
 
         ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
-        DESKTOP="$(bspc query -D -d '.focused' -m focused)"
 
         # turning sticky off before moving the node fixes the problem of the
         # scratchpad not being moved to a different monitor
@@ -35,7 +36,7 @@ case "$1" in
         bspc node -f "$ID"
         ;;
     hide)
-        bspc node --flag hidden
+        bspc node --flag hidden=on sticky=on
         ;;
     hide_floating)
         list_floating | while read -r F_ID; do
@@ -44,8 +45,12 @@ case "$1" in
         ;;
     unhide)
         list_hidden_no_scratchpad | while read -r H_ID; do
-            bspc node "$H_ID" --flag hidden
+            bspc node "$H_ID" --flag sticky=off
+            bspc node "$H_ID" -d "$DESKTOP"
+            bspc node "$H_ID" --flag hidden=off
+            bspc node "$H_ID" --flag sticky=on
             bspc node "$H_ID" --state floating
+            bspc node -f "$H_ID"
         done
         ;;
     *)
--- scripts/.local/share/scripts/toemacs.sh
+++ scripts/.local/share/scripts/toemacs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
+set -uo pipefail
 
 EMACS_DESKTOP=e
+SCRATCHCLASS=Emacs
 
 
 current_desktop() {
@@ -10,7 +11,11 @@ current_desktop() {
 }
 
 start_emacs() {
-    i3 workspace e
+    if pgrep -x bspwm; then
+        bspwm_workspace.sh switch $EMACS_DESKTOP
+    else
+        i3-msg workspace $EMACS_DESKTOP
+    fi
     emacs
 }
 
@@ -22,16 +27,60 @@ emacs_focused() {
     xprop WM_CLASS -id "$(xdotool getwindowfocus)" | grep -qF '"Emacs"'
 }
 
-if emacs_running; then
-    if emacs_focused; then
-        if [ "$(current_desktop)" = "$EMACS_DESKTOP" ]; then
-            i3-msg workspace back_and_forth
+workspace_back_and_forth() {
+    if pgrep -x bspwm; then
+        bspc desktop -f last
+    else
+        i3-msg workspace back_and_forth
+    fi
+}
+
+emacs_scratchpad_toggle() {
+    if pgrep -x bspwm; then
+        # copied from bspwm_scratchpad.sh
+        ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
+        DESKTOP="$(bspc query -D -d '.focused' -m focused)"
+        # turning sticky off before moving the node fixes the problem of the
+        # scratchpad not being moved to a different monitor
+        bspc node "$ID" --flag sticky=off
+        bspc node "$ID" -d "$DESKTOP"
+        bspc node "$ID" --flag sticky=on
+        bspc node "$ID" --flag hidden
+        bspc node -f "$ID"
+    else
+        # this actually hides emacs when it is inside the scratchpad at that time
+        i3-msg '[class="Emacs"] scratchpad show'
+    fi
+}
+
+list_hidden_bspwm() {
+    bspc query -N -n .hidden
+}
+
+
+emacs_window_focus() {
+    if pgrep -x bspwm; then
+        ID=$(xdotool search --class --classname $SCRATCHCLASS | head -1)
+
+        if list_hidden_bspwm | grep -qi "$(printf 0x0*%x "$ID")"; then
+            emacs_scratchpad_toggle
         else
-	    # this actually hides emacs when it is inside the scratchpad at that time
-	    i3-msg '[class="Emacs"] scratchpad show'
+            bspc node -f "$ID"
         fi
     else
         i3-msg '[class="Emacs"] focus'
+    fi
+}
+
+if emacs_running; then
+    if emacs_focused; then
+        if [ "$(current_desktop)" = "$EMACS_DESKTOP" ]; then
+            workspace_back_and_forth
+        else
+            emacs_scratchpad_toggle
+        fi
+    else
+        emacs_window_focus
     fi
 else
     start_emacs
