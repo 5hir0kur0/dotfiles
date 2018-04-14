3539212353ab0c158ccb2bf1d9b1de61977445c2
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-04-14 18:20:03 +0200

    center_window.sh: support multiple monitors

--- scripts/.local/share/scripts/center_window.sh
+++ scripts/.local/share/scripts/center_window.sh
@@ -1,31 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 
 monitor_info() {
-    xrandr | grep -F "$(bspc query -M -m focused --names)" -A3 | grep -F '*'
+    xrandr | grep -F "$(bspc query -M -m focused --names)" | grep -oP '\d+x\d+\+\d+\+\d+'
 }
 
 eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
-SCREEN_WIDTH=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f1)
-SCREEN_HEIGHT=$(monitor_info | grep -oP '\d+x\d+' | cut -dx -f2)
+MONINFO=$(monitor_info)
+SCREEN_WIDTH=$(grep -oP '\d+x\d+'       <<< "$MONINFO" | cut -dx -f1)
+SCREEN_HEIGHT=$(grep -oP '\d+x\d+'      <<< "$MONINFO" | cut -dx -f2)
+SCREEN_X_OFFSET=$(grep -oP '\+\d+\+\d+' <<< "$MONINFO" | cut -c2- | cut -d+ -f1)
+SCREEN_Y_OFFSET=$(grep -oP '\+\d+\+\d+' <<< "$MONINFO" | cut -c2- | cut -d+ -f2)
 
 case "$1" in
     ''|center)
-        xdotool windowmove "$WINDOW" "$(( (SCREEN_WIDTH / 2) - (WIDTH / 2) ))" \
-            "$(( (SCREEN_HEIGHT / 2) - (HEIGHT / 2) ))"
+        xdotool windowmove "$WINDOW" "$(( (SCREEN_WIDTH / 2) - (WIDTH / 2) + SCREEN_X_OFFSET ))" \
+            "$(( (SCREEN_HEIGHT / 2) - (HEIGHT / 2) + SCREEN_Y_OFFSET ))"
         ;;
     left)
-        xdotool windowmove "$WINDOW" $(( $(bspc config left_padding)  + $(bspc config window_gap) )) "$Y"
+        xdotool windowmove "$WINDOW" $(( $(bspc config left_padding) + $(bspc config window_gap) + SCREEN_X_OFFSET )) "$Y"
         ;;
     right)
         xdotool windowmove "$WINDOW" "$(( SCREEN_WIDTH - WIDTH - $(bspc config right_padding) \
-            - $(bspc config window_gap) - 2 * $(bspc config border_width) ))" "$Y"
+            - $(bspc config window_gap) - 2 * $(bspc config border_width) + SCREEN_X_OFFSET ))" "$Y"
         ;;
     top)
-        xdotool windowmove "$WINDOW" "$X" $(( $(bspc config top_padding) + $(bspc config window_gap) ))
+        xdotool windowmove "$WINDOW" "$X" $(( $(bspc config top_padding) + $(bspc config window_gap) + SCREEN_Y_OFFSET ))
         ;;
     bottom)
         xdotool windowmove "$WINDOW" "$X" "$(( SCREEN_HEIGHT - HEIGHT - $(bspc config window_gap) \
-            - $(bspc config bottom_padding) - 2 * $(bspc config border_width) ))"
+            - $(bspc config bottom_padding) - 2 * $(bspc config border_width) + SCREEN_Y_OFFSET ))"
+        ;;
+    *)
+        echo "usage: $0 [center, right, top, bottom]" 1>&2
+        exit 1
         ;;
 esac
