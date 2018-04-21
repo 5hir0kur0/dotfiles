31e2baef958b8dab16b2df1282bed1a0c72ce2fd
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-04-21 09:35:29 +0200

    blured_screenshot_lock.sh: adapt to new i3lock

--- scripts/.local/share/scripts/blured_screenshot_lock.sh
+++ scripts/.local/share/scripts/blured_screenshot_lock.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-colors=("--textcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "--insidecolor=1d202180" \
+colors=("--verifcolor=f9f5d7ff" "--wrongcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "--insidecolor=1d202180" \
         "--ringcolor=504945aa" "--linecolor=28282800" "--keyhlcolor=f9f5d7ee" \
         "--ringvercolor=458588ff" "--separatorcolor=28282800" \
         "--insidevercolor=1d2021aa" "--ringwrongcolor=cc241dff" \
@@ -11,6 +11,6 @@ scrot -q 100 /tmp/.screen_locked.png
 ffmpeg -i /tmp/.screen_locked.png -vf 'curves=increase_contrast, boxblur=9:9:lr=6:ar=6, curves=vintage' -y /tmp/.screen_locked.png
 
 pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=240 --datestr='%F' -k \
-    --datesize=30 --timesize=48 --timefont=Inconsolata --datefont=Inconsolata \
+    --datesize=30 --timesize=48 --time-font=Inconsolata --date-font=Inconsolata \
     --indicator --veriftext="..." --wrongtext="-_-" "${colors[@]}" \
-    --datepos='tx : ty + 35' --modsize=32 --textsize=32
+    --datepos='tx : ty + 35' --modsize=32 --verifsize=32 --wrongsize=32
