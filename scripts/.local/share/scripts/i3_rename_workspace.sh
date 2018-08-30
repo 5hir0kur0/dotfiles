ef17c81e8f1d8ac3421c1499aa39aeab4afeadee
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-08-30 16:29:48 +0200

    i3_rename_workspace.sh: fix whitespace-related bug
    * fix workspaces with spaces in their name not working correctly

--- scripts/.local/share/scripts/i3_rename_workspace.sh
+++ scripts/.local/share/scripts/i3_rename_workspace.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 
 WORKSPACE="$(rofi -dmenu -p 'rename workspace' < /dev/null)"
 
-[ -n "$WORKSPACE" ] && i3-msg rename workspace to "$WORKSPACE"
+[ -n "$WORKSPACE" ] && i3-msg rename workspace to "\"$WORKSPACE\""
