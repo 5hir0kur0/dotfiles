fff66ac494f4c42c611c59fdd1fbf10ec3f6f41e
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2018-12-06 16:08:05 +0100

    blured_screenshot_lock.sh: fix ffmpeg error

--- scripts/.local/share/scripts/blured_screenshot_lock.sh
+++ scripts/.local/share/scripts/blured_screenshot_lock.sh
@@ -6,9 +6,11 @@ colors=("--verifcolor=f9f5d7ff" "--wrongcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "
         "--insidevercolor=1d2021aa" "--ringwrongcolor=cc241dff" \
         "--insidewrongcolor=fb4934aa" "--datecolor=f9f5d7dd")
 
-scrot -q 100 /tmp/.screen_locked.png
+scrot -q 100 /tmp/.screen_locked_tmp.png
 #mogrify -blur 21x21 /tmp/.screen_locked.png
-ffmpeg -i /tmp/.screen_locked.png -vf 'curves=increase_contrast, boxblur=9:9:lr=6:ar=6, curves=vintage' -y /tmp/.screen_locked.png
+ffmpeg -i /tmp/.screen_locked_tmp.png -vf 'curves=increase_contrast, boxblur=9:9:lr=6:ar=6, curves=vintage' -y /tmp/.screen_locked.png
+
+rm -f /tmp/.screen_locked_tmp.png
 
 pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=240 --datestr='%F' -k \
     --datesize=30 --timesize=48 --time-font=Inconsolata --date-font=Inconsolata \
