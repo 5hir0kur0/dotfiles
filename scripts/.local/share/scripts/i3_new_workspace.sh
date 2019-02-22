920a97f03991f2cc8c7c61936fdad332324e90f3
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2019-02-22 21:49:02 +0100

    i3_new_workspace.sh: fix issue when there's no numerical workspace

--- scripts/.local/share/scripts/i3_new_workspace.sh
+++ scripts/.local/share/scripts/i3_new_workspace.sh
@@ -3,12 +3,13 @@
 set -euo pipefail
 
 list_workspaces() {
-    i3-msg -t get_workspaces | grep --color=never -oP '(?<="name":")\d+?(?=")'
+    # can fail if there are no numerical workspaces
+    i3-msg -t get_workspaces | grep --color=never -oP '(?<="name":")\d+?(?=")' || :
 }
 
 WORKSPACES="$(list_workspaces)"
 
-for (( num=1; num >= 0; ++num )); do
+for (( num=1; num <= 100; ++num )); do
     if ! grep -q "^$num\$" <<< "$WORKSPACES"; then
         i3-msg "${1-}" workspace "$num"
         exit
