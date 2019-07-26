52a69946cb1235ce8e50df6080a0837a4988f6c8
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2019-07-26 07:57:08 +0200

    mark.sh: fix wrong use of --add

--- scripts/.local/share/scripts/mark.sh
+++ scripts/.local/share/scripts/mark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
@@ -6,8 +6,9 @@ mark_exists() {
     i3-msg -t get_marks | grep -qF "\"${1?}\""
 }
 
-if mark_exists "${1?$0 needs a mark string as argument}"; then
+if mark_exists "${1:?$0 needs a mark string as argument}"; then
+    echo "window is already marked as ${1}" 1>&2
     exit 1
 else
-    i3-msg mark --add "${1?}"
+    i3-msg "mark --add ${1:?no mark string given}"
 fi
