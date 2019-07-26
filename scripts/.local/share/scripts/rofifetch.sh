1758938bfbcb4f96fef3813c34ccce8cbc448f2c
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2019-07-26 08:00:39 +0200

    rofifetch.sh: use rofi's own window mode (which has icons)
    (the downside is that windows on the current workspace are displayed as
    well)

--- scripts/.local/share/scripts/rofifetch.sh
+++ scripts/.local/share/scripts/rofifetch.sh
@@ -17,14 +17,27 @@ current_desktop() {
 }
 
 list_windows() {
-    wmctrl -l #| grep -vP ".+\s+$(current_desktop)\s+"
+    wmctrl -l | grep -vP ".+\s+$(current_desktop)\s+"
 }
 
-WINDOW=$(list_windows | rofi -dmenu -i -p 'fetch' | cut -f1 -d' ')
-
 if [[ "$(wm_name)" == bspwm ]]; then
+    WINDOW=$(list_windows | rofi -dmenu -i -p 'fetch' | cut -f1 -d' ')
     bspc node "$WINDOW" -d focused
     bspwm_workspace.sh reset
 else
+    # swap default action and alt action in rofi's window mode to allow 
+    # executing the command by default, the "Control+F13" is just a random 
+    # keybinding and not intended to be used
+    # TODO maybe exclude the current workspace
+    WINDOW=$(rofi -modi window -show window -kb-accept-entry 'Control+F13' \
+        -kb-accept-alt 'Return,Control+j,Control+m,KP_Enter' \
+        -window-command 'echo {window}')
+    # bring window to current desktop
     wmctrl -i -R "$WINDOW"
+    if [[ "$(wm_name)" == i3 ]]; then
+        # focus manually in i3
+        # (because currently I have my i3 set up to ignore focus requests...)
+        i3-msg "[id=${WINDOW}]" focus
+    fi
+
 fi
