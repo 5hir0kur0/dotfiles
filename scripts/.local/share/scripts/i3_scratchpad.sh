0194db901d7d8bdac3d9e1f4bdeea0740d04cfbb
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2019-10-19 18:50:22 +0200

    scratchpad.sh: make scratchpad work with urxvt again
    It still automatically launches st, but if urxvt is launched manually
    and no st scratchpads are present, the urxvt one is used.
    This is sometimes useful, because st has a bug that causes it to
    segfault when rendering certain characters.

--- scripts/.local/share/scripts/i3_scratchpad.sh
+++ scripts/.local/share/scripts/i3_scratchpad.sh
@@ -4,8 +4,10 @@
 # needs to be changed too
 SCRATCHCLASS='scratchpad-terminal'
 
-if xdotool search --class --classname "$SCRATCHCLASS" > /dev/null 2>&1; then
+if xdotool search --class "$SCRATCHCLASS" > /dev/null 2>&1; then
     i3-msg "[class=\"$SCRATCHCLASS\"] scratchpad show"
+elif xdotool search --classname "$SCRATCHCLASS" > /dev/null 2>&1; then
+    i3-msg "[instance=\"$SCRATCHCLASS\"] scratchpad show"
 else
     st -c "$SCRATCHCLASS" -e tmux new-session -As0
 fi
