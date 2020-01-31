94384e77317dad1fda4165b23d7df8099345567f
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2020-01-31 20:03:18 +0100

    init.sh: remove unsupported udiskie option

--- scripts/.local/share/scripts/init.sh
+++ scripts/.local/share/scripts/init.sh
@@ -8,7 +8,7 @@ pgrep -x nm-applet || { sleep 0.2; nm-applet; } &
 pgrep -x redshift-gtk || { sleep 0.5; redshift-gtk; } &
 pgrep -x keynav || { sleep 0.9; keynav; } &
 # i3 behaves buggy without noevents
-pgrep -x udiskie || { sleep 1; udiskie --use-udisks2 --no-automount --smart-tray; } &
+pgrep -x udiskie || { sleep 1; udiskie --no-automount --smart-tray --notify; } &
 pgrep -x unclutter || { sleep 5; unclutter -noevents -root -idle 8; } &
 pgrep -x xautolock || { sleep 10; xautolock -time 42 -locker "$HOME/.local/share/scripts/exit.sh lock"; } &
 
