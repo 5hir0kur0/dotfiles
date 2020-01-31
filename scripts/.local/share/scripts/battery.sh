d53403c2224bdc41a9aed19e1d58fc1a449bd598
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2020-01-31 20:04:00 +0100

    battery.sh: tweak values for warnings, add full notification
    * use a while loop instead of acpi_listen because the latter didn't seem
      to work properly

--- scripts/.local/share/scripts/battery.sh
+++ scripts/.local/share/scripts/battery.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-acpi_listen | while read -r event; do
-    if ! [[ "$event" =~ .*battery.* ]]; then
-        continue
-    fi
+while :; do
     udev=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
     level=$(grep percentage <<< "$udev" | grep -oP '\d+(?=%)')
     discharging=true
     if grep -qE 'state:\s*(charging|fully-charged)' <<< "$udev"; then
         discharging=false
     fi
-    if ((level <= 8)) && [ "$discharging" = true ]; then
+    if ((level <= 14)) && [[ "$discharging" == true ]]; then
         systemctl hibernate
-    elif ((level <= 20)) && [ "$discharging" = true ]; then
-        notify-send "low battery level: ${level}%"
-    # elif ((level >= 99)) && [ "$charging" = true ]; then
-    #     notify-send "battery full: ${level}%"
+    elif ((level <= 30)) && [[ "$discharging" == true ]]; then
+        notify-send -u critical "low battery level: ${level}%"
+    elif ((90 <= level && level <= 95)) && [[ "$discharging" == false ]]; then
+        notify-send -u low "battery full: ${level}%"
     fi
     # notify-send "battery: ${level}%"
+    sleep 3m
 done
