558988e71de137682f1eda28fbd812f39de62599
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2021-03-15 10:44:51 +0100

    blured_screenshot_lock: add exercise suggestions

--- scripts/.local/bin/blured_screenshot_lock.sh
+++ scripts/.local/bin/blured_screenshot_lock.sh
@@ -4,7 +4,7 @@ colors=("--verifcolor=f9f5d7ff" "--wrongcolor=f9f5d7ff" "--timecolor=f9f5d7ee" "
         "--ringcolor=504945aa" "--linecolor=28282800" "--keyhlcolor=f9f5d7ee" \
         "--ringvercolor=458588ff" "--separatorcolor=28282800" \
         "--insidevercolor=1d2021aa" "--ringwrongcolor=cc241dff" \
-        "--insidewrongcolor=fb4934aa" "--datecolor=f9f5d7dd")
+        "--insidewrongcolor=fb4934aa" "--datecolor=f9f5d7dd" "--greetercolor=f9f5d7ee")
 
 scrot -q 100 /tmp/.screen_locked_tmp.png
 #mogrify -blur 21x21 /tmp/.screen_locked.png
@@ -15,6 +15,7 @@ rm -f /tmp/.screen_locked_tmp.png
 pgrep -x i3lock || i3lock -i /tmp/.screen_locked.png -e --radius=240 --datestr='%F' -k \
     --datesize=30 --timesize=48 --time-font=Inconsolata --date-font=Inconsolata \
     --indicator --veriftext="..." --wrongtext="-_-" "${colors[@]}" \
-    --datepos='tx : ty + 35' --modsize=32 --verifsize=32 --wrongsize=32
+    --datepos='tx : ty + 35' --modsize=32 --verifsize=32 --wrongsize=32 \
+    --greetertext "how about: hindu pushups | pushup & rotation | burpees | jumping jacks | bicycle crunches | mountain climber";
 
 rm -f /tmp/.screen_locked.png
