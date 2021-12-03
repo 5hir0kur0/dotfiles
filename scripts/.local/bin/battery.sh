5960a79e06ce68ab611674e295cb00041b4b52fa
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2021-12-03 16:20:56 +0100

    battery.sh: remove 'almost full' notifications

--- scripts/.local/bin/battery.sh
+++ scripts/.local/bin/battery.sh
@@ -18,8 +18,6 @@ while :; do
         i3-nagbar -m "Battery critically low: ${level}%" -B Hibernate 'exit.sh hibernate' -B Suspend 'exit.sh suspend' -B 'Hybrid Sleep' 'exit.sh hybrid_sleep'
     elif ((level <= 30)) && [[ "$discharging" ]]; then
         notify-send -u critical "low battery level: ${level}%"
-    elif ((90 <= level && level <= 94)) && ! [[ "$discharging" ]]; then
-        notify-send -u low "battery almost full: ${level}%"
     fi
     # notify-send "battery: ${level}%"
     sleep 1m
