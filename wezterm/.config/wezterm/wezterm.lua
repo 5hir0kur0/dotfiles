9c5cab7b4c46741f168be8d4b68d13d2c8ad6f91
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2024-01-31 09:46:26 +0100

    wezterm: update config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,18 +1,23 @@
 local wezterm = require 'wezterm'
 return {
-    scrollback_lines = 10000,
     font =  wezterm.font_with_fallback {
-        -- 'Monaspace Xenon',
+       -- 'Monaspace Xenon',
         {
             family = 'Iosevka Term Slab',
-            -- harfbuzz_features = { '+ss07' },
+            harfbuzz_features = { '+ss07' },
         },
         'Noto Serif CJK JP',
     },
-    font_size = 16.0,
-    color_scheme = 'GruvboxDarkHard',
+    font_size = 17,
+    -- color_scheme = 'Solarized Dark Higher Contrast (Gogh)',
+    -- color_scheme = 'GruvboxDarkHard',
+    color_scheme = 'Gruvbox dark, hard (base16)',
     default_cursor_style = 'SteadyBar',
     hide_tab_bar_if_only_one_tab = true,
+    force_reverse_video_cursor = true,
+    scrollback_lines = 10000,
+    -- scroll less fast (useful when scrolling via touchpad)
+    alternate_buffer_wheel_scroll_speed = 1,
     window_decorations = "RESIZE",
     window_padding = {
         left = 0,
