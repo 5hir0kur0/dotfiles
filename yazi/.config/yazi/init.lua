d340d77344be7bc6b22dfa1e53457e3c1d14682d
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2025-04-17 13:32:52 +0900

    Fix yazi config

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,11 +1,12 @@
+-- Add more spacing after icons
 function Entity:icon()
 	local icon = self._file:icon()
 	if not icon then
-		return ui.Line("")
-	elseif self._file:is_hovered() then
-		return ui.Line(" " .. icon.text .. "  ")
+		return ""
+	elseif self._file.is_hovered then
+		return icon.text .. "  "
 	else
-		return ui.Line(" " .. icon.text .. "  "):style(icon.style)
+		return ui.Line(icon.text .. "  "):style(icon.style)
 	end
 end
 
