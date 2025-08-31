b789cf79f5d93f7042fc26cf40378d6b632ef137
5hir0kur0 <12101162+5hir0kur0@users.noreply.github.com>
2025-08-31 21:49:12 +0900

    doom: remove obsolete +ranger option

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -72,7 +72,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +ranger +icons)   ; making dired pretty [functional]
+       (dired +icons)   ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;eww               ; the internet is gross
        ibuffer           ; interactive buffer management
